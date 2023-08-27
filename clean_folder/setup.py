25b9b4c64f6a7da44a7872ab4ea992bf3edea4ed
VadimR89 <rabtsun.vadim@gmail.com>
2023-08-27 16:55:54 +0300

    Library_Clean_folder

---
+++ clean_folder/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='Clean_folder',
+    version='0.0.1',
+    description='Very useful code',
+    url='https://github.com/VadimR89/File_Organizer',
+    author='VadimR89',
+    author_email='rabtsun.vadim@gmail.com',
+    license='MIT',
+    packages=['clean_folder'],
+    install_requires=['markdown'],
+)
